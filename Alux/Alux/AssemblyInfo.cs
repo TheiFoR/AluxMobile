942b3fd4707b1bcf5867f2168a2dc4178c37dd30
TheiFoR <igorbutorin.nk@gmail.com>
2023-01-11 13:53:12 +0300

    Добавьте файлы проекта.

---
+++ Alux/Alux/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Glyphter.ttf", Alias = "iconFonts1")]
+[assembly: ExportFont("Glyphter2.ttf", Alias = "iconFonts2")]
+[assembly: ExportFont("Roboto-Black.ttf", Alias = "RobotoBlack")]
+[assembly: ExportFont("Roboto-Medium.ttf", Alias = "RobotoMedium")]
+[assembly: ExportFont("Roboto-Light.ttf", Alias = "RobotoLight")]
---
+++ Alux/Alux/View/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Alux
+{
+    public partial class MainPage: ContentPage
+    {
+        string selectedIconColor = "#31acec";
+        string IconColor = "#808080";
+
+        float selectedScale = 0.9f;
+        uint lengthAnimation = 75;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            onOrOffSettings.Pressed += panelButtonPress;
+            onOrOffSettings.Released += panelButtonRelease;
+
+            brightSettings.Pressed += panelButtonPress;
+            brightSettings.Released += panelButtonRelease;
+
+            effectSettings.Pressed += panelButtonPress;
+            effectSettings.Released += panelButtonRelease;
+
+            moreSettings.Pressed += panelButtonPress;
+            moreSettings.Released += panelButtonRelease;
+        }
+        async void panelButtonPress(object sender, EventArgs args)
+        {
+            Element s = (sender as Button).Parent;
+            await (s as Grid).ScaleTo(selectedScale, lengthAnimation);
+        }
+        async void panelButtonRelease(object sender, EventArgs args)
+        {
+            Element s = (sender as Button).Parent;
+            await (s as Grid).ScaleTo(1, lengthAnimation);
+        }
+        void onOrOffLights(object sender, EventArgs args)
+        {
+            
+        }
+        void goToMainPage(object sender, EventArgs args)
+        {
+            homePage.TextColor = Color.FromHex(selectedIconColor);
+            selectionPage.TextColor = Color.FromHex(IconColor);
+            settingsPage.TextColor = Color.FromHex(IconColor);
+        }
+        void goToModeSelectionPage(object sender, EventArgs args)
+        {
+            homePage.TextColor = Color.FromHex(IconColor);
+            selectionPage.TextColor = Color.FromHex(selectedIconColor);
+            settingsPage.TextColor = Color.FromHex(IconColor);
+        }
+        void goToSettings(object sender, EventArgs args)
+        {
+            homePage.TextColor = Color.FromHex(IconColor);
+            selectionPage.TextColor = Color.FromHex(IconColor);
+            settingsPage.TextColor = Color.FromHex(selectedIconColor);
+        }
+    }
+}
