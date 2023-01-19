7b989f608f83aef338305374a80e6c8e686828b3
TheiFoR <igorbutorin.nk@gmail.com>
2023-01-19 20:43:32 +0300

    .

--- Alux/Alux/App.xaml.cs
+++ Alux/Alux/App.xaml.cs
@@ -10,7 +10,7 @@ namespace Alux
         public App()
         {
             InitializeComponent();
-            MainPage = new ViewingBluetoothDevices();
+            MainPage = new MainPage();
         }
 
         protected override void OnStart()
